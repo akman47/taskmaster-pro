1c31ceaee8c139b4088a07fa532c159757b8a1a8
Angela Man <angela.k.man@gmail.com>
2021-04-10 09:27:38 -0500

    turn trash into a droppable

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,6 +199,22 @@ $(".card .list-group").sortable({
     }
 });
 
+// make trash droppable
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+        console.log("drop");
+        ui.draggable.remove();
+    },
+    over: function(event, ui) {
+        console.log("over");
+    },
+    out: function(event, ui) {
+        console.log("out");
+    }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
